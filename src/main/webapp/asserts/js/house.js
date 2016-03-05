6d470d3a03232347fdeefbb22042989cf45e34e1
张达 <da.zhang@wz-inc.com>
2016-03-05 23:49:28 +0800

    add house js

--- src/main/webapp/asserts/js/house.js
+++ src/main/webapp/asserts/js/house.js
@@ -1,60 +1,75 @@
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+
+
 var SchoolList = React.createClass({
-  getInitialState: function() {
-    return {
-      schools : []
-    };
-  },
+    getInitialState: function() {
+        return {
+            schools : []
+        };
+    },
 
-  componentDidMount: function() {
-    /*$.ajax({
-      url: this.props.source,
-      success: function(data) {
-        console.log(data);
+    componentDidMount: function() {
+        /*$.ajax({
+         url: this.props.source,
+         success: function(data) {
+         console.log(data);
 
-        this.setState({schools: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(err.toString());
-      }.bind(this)
-    });*/
+         this.setState({schools: data});
+         }.bind(this),
+         error: function(xhr, status, err) {
+         console.error(err.toString());
+         }.bind(this)
+         });*/
 
-    $.get(this.props.source, function(result) {
-        console.log(result);
-        this.setState({schools : result});
-    }.bind(this));
-  },
+        $.get(this.props.source, function(result) {
+            console.log(result);
+            this.setState({schools : result});
+        }.bind(this));
+    },
 
-  render: function() {
-    //console.log(this.state.schools);
-    return (
-        <table className="table table-bordered table-striped">
-          <thead>
-          <tr>
-            <th>
-              学校名称
-            </th>
-            <th>
-              地址
-            </th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            this.state.schools.map(function (row,i) {   //这里因为data是个数组，所以可以用map来遍历
-              return (
-                  <tr key={i}>
-                    <td><a href={"/pages/estate.jsp?id=" + row.id} >{row.name}</a></td>  //取特定的值
-                    <td>{row.address}</td>
-                  </tr>
-              );
-            })
-          }
-          </tbody></table>
-    );
-  }
+    render: function() {
+        //console.log(this.state.schools);
+        return (
+            <table className="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>
+                        小区名称
+                    </th>
+                    <th>
+                        面积
+                    </th>
+                    <th>
+                        价格
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    this.state.schools.map(function (row,i) {   //这里因为data是个数组，所以可以用map来遍历
+                        return (
+                            <tr key={i}>
+                                <td><a href="#" >{row.title}</a></td>  //取特定的值
+                                <td>{row.area}</td>
+                                <td>{row.price}</td>
+                            </tr>
+                        );
+                    })
+                }
+                </tbody></table>
+        );
+    }
 });
 
-ReactDOM.render(
-    <SchoolList source="/schoolList.json" />,
-    document.getElementById("example")
-);
+$(document).ready(function() {
+    var estateID = getQueryString("id");
+    ReactDOM.render(
+        <SchoolList source={"/house/list.json?id=" + estateID} />,
+        document.getElementById("example")
+    );
+});
+
